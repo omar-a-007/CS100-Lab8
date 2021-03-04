3a792006af437c53cefa11f7ddb5078152dd7eed
oalku001 <oalku001@ucr.edu>
2021-03-03 23:17:29 -0800

    Bug fix in output

--- headers/visitMathML.hpp
+++ headers/visitMathML.hpp
@@ -9,20 +9,20 @@
 
 class VisitMathML : public Visitor {
     private:
+        std::string print;
         std::stringstream stream;
-        unsigned indent;
-        std::string repeat(unsigned num, const std::string& input = "  ")
+        int indent;
+        std::string repeat(int num, const std::string& input = "  ")
         {
             std::string ret;
-            //for (num; num > 0; num--) ret += input;
             while (num--) ret += input;
             return ret;
         }
     public:
         std::string PrintMathML(Base* ptr)
         {
-            std::stringstream().swap(stream); // Clears the stringstreams error stat and emptys its contents
-            stream << "<math>\n";
+            print = "";
+            print += "<math>\n";
             indent = 1;
             Iterator iter (ptr);
             while (!iter.is_done())
@@ -30,22 +30,22 @@ class VisitMathML : public Visitor {
                 iter.current_node()->accept(this, iter.current_index());
 		        iter.next();
 		    }
-            stream << "</math>\n";
-            return stream.str();
+            print += "</math>\n";
+            return print;
         }
-        virtual void visit_rand(Rand* node)        { stream << repeat(indent) << "<cn>" + node->print() << "</cn>\n"; }
-        virtual void visit_op(Op* node)            { stream << repeat(indent) << "<cn>" + node->print() << "</cn>\n"; }
-        virtual void visit_add_begin(Add* node)    { stream << repeat(indent) << "<apply>\n" << repeat(++indent) << "<plus/>\n"  ; }
-        virtual void visit_sub_begin(Sub* node)    { stream << repeat(indent) << "<apply>\n" << repeat(++indent) << "<minus/>\n" ; }       
-        virtual void visit_mult_begin(Mult* node)  { stream << repeat(indent) << "<apply>\n" << repeat(++indent) << "<times/>\n" ; }       
-        virtual void visit_div_begin(Div* node)    { stream << repeat(indent) << "<apply>\n" << repeat(++indent) << "<divide/>\n"; }
-        virtual void visit_pow_begin(Pow* node)    { stream << repeat(indent) << "<apply>\n" << repeat(++indent) << "<power/>\n" ; }
+        virtual void visit_rand(Rand* node)        { print += repeat(indent) + "<cn>" + node->print() + "</cn>\n"; }
+        virtual void visit_op(Op* node)            { print += repeat(indent) + "<cn>" + node->print() + "</cn>\n"; }
+        virtual void visit_add_begin(Add* node)    { print += repeat(indent) + "<apply>\n"; ++indent; print += repeat(indent) + "<plus/>\n"  ; }
+        virtual void visit_sub_begin(Sub* node)    { print += repeat(indent) + "<apply>\n"; ++indent; print += repeat(indent) + "<minus/>\n" ; }       
+        virtual void visit_mult_begin(Mult* node)  { print += repeat(indent) + "<apply>\n"; ++indent; print += repeat(indent) + "<times/>\n" ; }       
+        virtual void visit_div_begin(Div* node)    { print += repeat(indent) + "<apply>\n"; ++indent; print += repeat(indent) + "<divide/>\n"; }
+        virtual void visit_pow_begin(Pow* node)    { print += repeat(indent) + "<apply>\n"; ++indent; print += repeat(indent) + "<power/>\n" ; }
                             
-        virtual void visit_add_end(Add* node)      { stream << repeat(--indent) << "</apply>\n"; }
-        virtual void visit_sub_end(Sub* node)      { stream << repeat(--indent) << "</apply>\n"; }
-        virtual void visit_mult_end(Mult* node)    { stream << repeat(--indent) << "</apply>\n"; }
-        virtual void visit_div_end(Div* node)      { stream << repeat(--indent) << "</apply>\n"; }
-        virtual void visit_pow_end(Pow* node)      { stream << repeat(--indent) << "</apply>\n"; }
+        virtual void visit_add_end(Add* node)      { print += repeat(--indent) + "</apply>\n"; }
+        virtual void visit_sub_end(Sub* node)      { print += repeat(--indent) + "</apply>\n"; }
+        virtual void visit_mult_end(Mult* node)    { print += repeat(--indent) + "</apply>\n"; }
+        virtual void visit_div_end(Div* node)      { print += repeat(--indent) + "</apply>\n"; }
+        virtual void visit_pow_end(Pow* node)      { print += repeat(--indent) + "</apply>\n"; }
 
         virtual void visit_add_middle(Add* node)   { }
         virtual void visit_sub_middle(Sub* node)   { }
